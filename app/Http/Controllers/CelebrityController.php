5023c6289d4707fc0855e1da603071b516c2c712
Maxime <maxime.autechaud@gmail.com>
2021-10-19 15:09:01 +0200

    phpdoc + comments

--- app/Http/Controllers/CelebrityController.php
+++ app/Http/Controllers/CelebrityController.php
@@ -7,17 +7,30 @@ use Illuminate\Http\Request;
 
 class CelebrityController extends Controller
 {
+    /**
+     * Récupère le sitemsen DB pour les afficher dans l'index
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
     public function index()
     {
         $celebrities = Celebrity::orderBy('lastname')->get();
         return view('index', compact("celebrities"));
     }
 
+    /**
+     * Vue du formulaire
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
     public function create()
     {
         return view('form');
     }
 
+    /**
+     * Verif serveur selon critère puis valide la création de l'item
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function store(Request $request)
     {
         $request->validate([
@@ -30,6 +43,12 @@ class CelebrityController extends Controller
         return redirect()->route("home");
     }
 
+
+    /**
+     * Récupère l'ID de l'item pour delete en DB
+     * @param $id
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function delete($id)
     {
         $celebrity = Celebrity::find($id);
@@ -37,12 +56,22 @@ class CelebrityController extends Controller
         return redirect()->route("home");
     }
 
+    /**
+     * Renvoi à la vue du formulaire pré-rempli des informations de l'item sélectionné
+     * @param $id
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
     public function edit($id)
     {
         $celebrity = Celebrity::find($id);
         return view("edit", compact('celebrity'));
     }
 
+    /**
+     * Met à jour en DB après verif du formulaire
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function update(Request $request)
     {
         $request->validate([
